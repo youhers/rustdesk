61d102b873f45059978184884050dbeab07d20e6
youhers <106003909+youhers@users.noreply.github.com>
2025-04-06 21:28:15 +0800

    Update cn.rs

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -5,7 +5,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Your Desktop", "你的桌面"),
         ("desk_tip", "你的桌面可以通过下面的 ID 和密码访问。"),
         ("Password", "密码"),
-        ("Ready", "就绪"),
+        ("Ready", "已连接安全加密链路"),
         ("Established", "已建立"),
         ("connecting_status", "正在接入 RustDesk 网络..."),
         ("Enable service", "允许服务"),
